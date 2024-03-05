51163398ce0dc2556643adfb2cf39a261049dca8
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-03-04 20:09:13 -0600

    User model init

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const bcrypt = require("./bcrypt");
+const Order = require("./Order");
+
+// User Schema
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  username: { type: String, required: true },
+  email: { type: String, unique: true, lowercase: true }, //email address of the user
+  password: {
+    type: String,
+    required: "Please provide a password",
+    minlength: 6,
+  },
+  orders: [Order.schema],
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+//Compare the incoming password the hashed password
+userSchema.methods.isCorrectPassoword = async function (passowrd) {
+  return await bcrypt.compare(password, this.passowrd);
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
