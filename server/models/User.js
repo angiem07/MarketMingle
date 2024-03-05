dac648fc163bc9dfdd66b4a81656045835111d1e
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-03-04 20:11:58 -0600

    misspelled something

--- server/models/User.js
+++ server/models/User.js
@@ -26,7 +26,7 @@ userSchema.pre("save", async function (next) {
 });
 
 //Compare the incoming password the hashed password
-userSchema.methods.isCorrectPassoword = async function (passowrd) {
+userSchema.methods.isCorrectPassoword = async function (password) {
   return await bcrypt.compare(password, this.passowrd);
 };
 const User = mongoose.model("User", userSchema);
