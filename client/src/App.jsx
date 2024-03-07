94a16b9fca6fefe19370061b8fb487513d295aee
Angie <angelam202@hotmail.com>
2024-03-06 21:09:30 -0600

    work on making login the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,11 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-
-// import "./App.css";
-
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            {/* Define other routes as needed */}
-          </Routes>
-        </div>
-    </ApolloProvider>
+    <div className="flex-column justify-center align-center min-100-vh bg-primary">
+      <Outlet />
+    </div>
   );
 }
 
