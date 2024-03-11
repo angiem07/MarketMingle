6fe4ba867befdf4232f23425f56b2040ff36d345
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-03-11 16:14:18 -0500

    adding categories

--- client/src/tests/reducers.test.jsx
+++ client/src/tests/reducers.test.jsx
@@ -94,25 +94,25 @@ test("ADD_MULTIPLE_TO_CART", () => {
   expect(initialState.cart.length).toBe(2);
 });
 
-// test("UPDATE_CATEGORIES", () => {
-//   let newState = reducer(initialState, {
-//     type: UPDATE_CATEGORIES,
-//     categories: [{}, {}],
-//   });
+test("UPDATE_CATEGORIES", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}],
+  });
 
-//   expect(newState.categories.length).toBe(2);
-//   expect(initialState.categories.length).toBe(1);
-// });
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
 
-// test("UPDATE_CURRENT_CATEGORY", () => {
-//   let newState = reducer(initialState, {
-//     type: UPDATE_CURRENT_CATEGORY,
-//     currentCategory: "2",
-//   });
+test("UPDATE_CURRENT_CATEGORY", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: "2",
+  });
 
-//   expect(newState.currentCategory).toBe("2");
-//   expect(initialState.currentCategory).toBe("1");
-// });
+  expect(newState.currentCategory).toBe("2");
+  expect(initialState.currentCategory).toBe("1");
+});
 
 test("CLEAR_CART", () => {
   let newState = reducer(initialState, {
