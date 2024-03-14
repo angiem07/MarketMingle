ec8f76bb08cf94f3bb6dfc53e900ee4cb1ffdd60
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-03-13 23:44:52 -0500

    fix all

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/market"
+  process.env.MONGODB_URI ||
+    "mongodb+srv://bcort49:lYXyAEjOJkxfWCTy@marketmingle.3jhy40d.mongodb.net/"
 );
 
 module.exports = mongoose.connection;
+
+//mongodb+srv://bcort49:lYXyAEjOJkxfWCTy@marketmingle.3jhy40d.mongodb.net/
+//mongodb+srv://bcort49:<password>@marketmingle.3jhy40d.mongodb.net/
+// lYXyAEjOJkxfWCTy;
