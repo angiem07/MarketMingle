f412e90e2d6a8f5235c6ad57fc0598df33ecaf90
Angie <angelam202@hotmail.com>
2024-03-13 22:58:54 -0500

    work on atlas connection and deploy in render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://angelam2326:WkG3J0aoKF1Vnhc0@marketmingle.sldbah1.mongodb.net/market');
 
 module.exports = mongoose.connection;
-g
